9520855ce3032932e3a7b78fcb88743b43ce1bf0
Olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2025-07-18 12:25:10 +0100

    hero.tsx

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -3,8 +3,8 @@ import {} from 'react'
 export default function Example() {
   return (
     <>
-      <div className="mx-auto max-w-2xl pb-40 py-32 sm:py-48 lg:py-56 max-sm:py-20 max-md:py-28 px-4 max-sm:px-3 max-md:px-6">
-        <div className="text-center">
+     
+        <div className="text-center mt-25 px-4 max-sm:px-2 max-md:px-4">
           <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl max-sm:text-3xl max-md:text-4xl">
             Explore the world with comfort, safety & style ✈️
           </h1>
@@ -14,13 +14,13 @@ export default function Example() {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#destinations"
-              className="rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-xs hover:bg-lime-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600 max-sm:px-3 max-sm:py-2 max-sm:text-xs max-md:text-sm"
+              className="rounded-md bg-lime-500 px-3.5 mb-8 py-2.5 text-sm font-semibold text-black shadow-xs hover:bg-lime-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600 max-sm:px-3 max-sm:py-2 max-sm:text-xs max-md:text-sm"
             >
               Start Your Journey
             </a>
           </div>
         </div>
-      </div>
+     
 
       {/* Decorative background blur (optional) */}
       <div
